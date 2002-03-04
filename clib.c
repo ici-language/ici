9baf6c3ee8f14ac53a84e5ff61434ee78f889aa2
Tim Long <timl@sourceforge.net>
2002-03-04 12:46:23 +0000

    Added dir(), chdir(), getcwd(), and rename() back into the core. They got removed when syscall was removed, but they should be core features.

--- clib.c
+++ clib.c
@@ -7,9 +7,44 @@
 #include "float.h"
 #include "str.h"
 #include "buf.h"
+#include "null.h"
+#include "re.h"
 #ifndef NOSKT
 #include "skt.h"
 #endif
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+#if defined(sun) || defined(BSD4_4) || defined(linux)
+#include <sys/wait.h>
+#endif
+#ifdef NeXT
+#include <libc.h>
+#endif
+
+#ifndef NODIR
+#ifndef _WIN32
+#include <sys/param.h>
+#include <dirent.h>
+#endif   /* _WIN32 */
+#endif   /* NODIR */
+
+#if defined(linux) && !defined(MAXPATHLEN)
+#include <sys/param.h>
+#endif
+
+#ifdef _WIN32
+#include <io.h>
+#include <process.h>
+#include <direct.h>
+#endif
+
 
 /*
  * C library and others.  We don't want to go overboard here.  Would
@@ -479,6 +514,346 @@ f_remove(void)
     return null_ret();
 }
 
+#ifndef NODIR
+
+#ifdef _WIN32
+/*
+ * Emulate opendir/readdir/et al under WIN32 environments via findfirst/
+ * findnext. Only what f_dir() needs has been emulated (which is to say,
+ * not much).
+ */
+
+#define MAXPATHLEN      _MAX_PATH
+
+struct dirent
+{
+    char        *d_name;
+};
+
+typedef struct DIR
+{
+    long                handle;
+    struct _finddata_t  finddata;
+    int                 needfindnext;
+    struct dirent       dirent;
+}
+DIR;
+
+static DIR *
+opendir(const char *path)
+{
+    DIR         *dir;
+    char        fspec[_MAX_PATH+1];
+
+    if (strlen(path) > (_MAX_PATH - 4))
+        return NULL;
+    sprintf(fspec, "%s/*.*", path);
+    if ((dir = ici_talloc(DIR)) != NULL)
+    {
+        if ((dir->handle = _findfirst(fspec, &dir->finddata)) == -1)
+        {
+            ici_tfree(dir, DIR);
+            return NULL;
+        }
+        dir->needfindnext = 0;
+    }
+    return dir;
+}
+
+static struct dirent *
+readdir(DIR *dir)
+{
+    if (dir->needfindnext && _findnext(dir->handle, &dir->finddata) != 0)
+            return NULL;
+    dir->dirent.d_name = dir->finddata.name;
+    dir->needfindnext = 1;
+    return &dir->dirent;
+}
+
+static void
+closedir(DIR *dir)
+{
+    _findclose(dir->handle);
+    ici_tfree(dir, DIR);
+}
+
+#define S_ISREG(m)      (((m) & _S_IFMT) == _S_IFREG)
+#define S_ISDIR(m)      (((m) & _S_IFMT) == _S_IFDIR)
+
+#endif
+
+/*
+ * array = dir([path] [, regexp] [, format])
+ *
+ * Read directory named in path (a string, defaulting to ".", the current
+ * working directory) and return the entries that match the pattern (or
+ * all names if no pattern passed). The format string identifies what
+ * sort of entries should be returned. If the format string is passed
+ * then a path MUST be passed (to avoid any ambiguity) but path may be
+ * NULL meaning the current working directory (same as "."). The format
+ * string uses the following characters,
+ *
+ *      f       return file names
+ *      d       return directory names
+ *      a       return all names (which includes things other than
+ *              files and directories, e.g., hidden or special files
+ *
+ * The default format specifier is "f".
+ */
+static int
+f_dir(void)
+{
+    char                *path   = ".";
+    char                *format = "f";
+    regexp_t            *regexp = NULL;
+    object_t            *o;
+    array_t             *a;
+    DIR                 *dir;
+    struct dirent       *dirent;
+    int                 fmt;
+    string_t            *s;
+
+    switch (NARGS())
+    {
+    case 0:
+        break;
+
+    case 1:
+        o = ARG(0);
+        if (isstring(o))
+            path = stringof(o)->s_chars;
+        else if (isnull(o))
+            ;   /* leave path as is */
+        else if (isregexp(o))
+            regexp = regexpof(o);
+        else
+            return ici_argerror(0);
+        break;
+
+    case 2:
+        o = ARG(0);
+        if (isstring(o))
+            path = stringof(o)->s_chars;
+        else if (isnull(o))
+            ;   /* leave path as is */
+        else if (isregexp(o))
+            regexp = regexpof(o);
+        else
+            return ici_argerror(0);
+        o = ARG(1);
+        if (isregexp(o))
+        {
+            if (regexp != NULL)
+                return ici_argerror(1);
+            regexp = regexpof(o);
+        }
+        else if (isstring(o))
+            format = stringof(o)->s_chars;
+        else
+            return ici_argerror(1);
+        break;
+
+    case 3:
+        o = ARG(0);
+        if (isstring(o))
+            path = stringof(o)->s_chars;
+        else if (isnull(o))
+            ;   /* leave path as is */
+        else
+            return ici_argerror(0);
+        o = ARG(1);
+        if (!isregexp(o))
+            return ici_argerror(1);
+        regexp = regexpof(o);
+        o = ARG(2);
+        if (!isstring(o))
+            return ici_argerror(2);
+        format = stringof(o)->s_chars;
+        break;
+
+    default:
+        return ici_argcount(3);
+    }
+
+    if (*path == '\0')
+        path = ".";
+
+#define FILES   1
+#define DIRS    2
+#define OTHERS  4
+
+    for (fmt = 0; *format != '\0'; ++format)
+    {
+        switch (*format)
+        {
+        case 'f':
+            fmt |= FILES;
+            break;
+
+        case 'd':
+            fmt |= DIRS;
+            break;
+
+        case 'a':
+            fmt |= OTHERS | DIRS | FILES;
+            break;
+
+        default:
+            ici_error = "bad directory format specifier";
+            return 1;
+        }
+    }
+    if ((a = new_array(0)) == NULL)
+        return 1;
+    if ((dir = opendir(path)) == NULL)
+    {
+        if (ici_chkbuf(strlen(path) + 32))
+            ici_error = syserr();
+        else
+        {
+            sprintf(buf, "%s: %s", path, syserr());
+            ici_error = buf;
+        }
+        goto fail;
+    }
+    while ((dirent = readdir(dir)) != NULL)
+    {
+        struct stat     statbuf;
+        char            abspath[MAXPATHLEN+1];
+
+        if
+        (
+            regexp != NULL
+            &&
+            pcre_exec
+            (
+                regexp->r_re,
+                regexp->r_rex,
+                dirent->d_name,
+                strlen(dirent->d_name),
+                0,
+                0,
+                re_bra,
+                nels(re_bra)
+            )
+            < 0
+        )
+            continue;
+        sprintf(abspath, "%s/%s", path, dirent->d_name);
+#ifndef _WIN32
+        if (lstat(abspath, &statbuf) == -1)
+        {
+            if (ici_chkbuf(strlen(abspath) + 32))
+                ici_error = syserr();
+            else
+            {
+                sprintf(buf, "%s: %s", abspath, syserr());
+                ici_error = buf;
+            }
+            closedir(dir);
+            goto fail;
+        }
+        if (S_ISLNK(statbuf.st_mode) && stat(abspath, &statbuf) == -1)
+            continue;
+#else
+        if (stat(abspath, &statbuf) == -1)
+            continue;
+#endif
+        if
+        (
+            (S_ISREG(statbuf.st_mode) && fmt & FILES)
+            ||
+            (S_ISDIR(statbuf.st_mode) && fmt & DIRS)
+            ||
+            fmt & OTHERS
+        )
+        {
+            if
+            (
+                (s = new_cname(dirent->d_name)) == NULL
+                ||
+                ici_stk_push_chk(a, 1)
+            )
+            {
+                if (s != NULL)
+                    decref(s);
+                closedir(dir);
+                goto fail;
+            }
+            *a->a_top++ = objof(s);
+            decref(s);
+        }
+    }
+    closedir(dir);
+    return ici_ret_with_decref(objof(a));
+
+#undef  FILES
+#undef  DIRS
+#undef  OTHERS
+
+fail:
+    decref(a);
+    return 1;
+}
+#endif  /* NODIR */
+
+/*
+ * Used as a common error return for system calls that fail. Sets the
+ * global error string if the call fails otherwise returns the integer
+ * result of the system call.
+ */
+static int
+sys_ret(ret)
+int     ret;
+{
+    if (ret < 0)
+    {
+        ici_error = syserr();
+        return 1;
+    }
+    return int_ret((long)ret);
+}
+
+/*
+ * rename(oldpath, newpath)
+ */
+static int
+f_rename()
+{
+    char                *o;
+    char                *n;
+
+    if (ici_typecheck("ss", &o, &n))
+        return 1;
+    return sys_ret(rename(o, n));
+}
+
+/*
+ * chdir(newdir)
+ */
+static int
+f_chdir()
+{
+    char                *n;
+
+    if (ici_typecheck("s", &n))
+        return 1;
+    return sys_ret(chdir(n));
+}
+
+/*
+ * string = getcwd()
+ */
+static int
+f_getcwd(void)
+{
+    char        buf[MAXPATHLEN+1];
+
+    if (getcwd(buf, sizeof buf) == NULL)
+        return sys_ret(-1);
+    return str_ret(buf);
+}
+
 cfunc_t clib_cfuncs[] =
 {
     {CF_OBJ,    "printf",       f_sprintf,      (int (*)())1},
@@ -498,6 +873,12 @@ cfunc_t clib_cfuncs[] =
 #endif
     {CF_OBJ,    "eof",          f_eof},
     {CF_OBJ,    "remove",       f_remove},
+#ifndef NODIR
+    {CF_OBJ,    "dir",          f_dir},
+#endif
+    {CF_OBJ,    "getcwd",       f_getcwd},
+    {CF_OBJ,    "chdir",        f_chdir},
+    {CF_OBJ,    "rename",       f_rename},
 
     {CF_OBJ}
 };
