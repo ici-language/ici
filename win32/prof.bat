8f3b7a6b0dc93b9031c59c41f6ee2055d875ec52
Tim Long <timl@sourceforge.net>
2002-12-24 03:35:02 +0000

    Added Windows build support based on MS Visual C.

---
+++ win32/prof.bat
@@ -0,0 +1,5 @@
+PREP /OM Release\ici.exe Release\ici4.dll
+PROFILE Release\ici.exe C:\timl\ICI\shoot\bench\wordfreq\wordfreq.ici < C:\timl\ICI\shoot\bench\wordfreq\Input > C:\timl\ICI\shoot\bench\wordfreq\xxxi
+PREP /M  Release\ici
+PLIST /st Release\ici > prof.out
+
