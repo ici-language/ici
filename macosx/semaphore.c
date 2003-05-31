4ef528fc8d68aada27c92835000acfd941228dca
Andy Newman <atrn@sourceforge.net>
2003-05-31 20:57:41 +0000

    Remove old error string setting. Rely upon pthread's setting errno for later pick up by the core interpreter code.

--- macosx/semaphore.c
+++ macosx/semaphore.c
@@ -15,14 +15,10 @@ sem_init(sem_t *sem, int pshared, unsigned int count)
     (void)pshared;
 
     if (pthread_mutexattr_init(&mutex_attr) == -1)
-    {
-	ici_get_last_errno("mutexattr_init", NULL);
 	return -1;
-    }
     pthread_mutexattr_settype(&mutex_attr, PTHREAD_MUTEX_RECURSIVE);
     if (pthread_mutex_init(&sem->sem_mutex, &mutex_attr) == -1)
     {
-	ici_get_last_errno("mutex_init", NULL);
 	pthread_mutexattr_destroy(&mutex_attr);
 	return -1;
     }
@@ -30,7 +26,6 @@ sem_init(sem_t *sem, int pshared, unsigned int count)
     pthread_condattr_init(&condvar_attr);
     if (pthread_cond_init(&sem->sem_condvar, &condvar_attr) == -1)
     {
-	ici_get_last_errno("cond_init", NULL);
 	pthread_condattr_destroy(&condvar_attr);
 	pthread_mutex_destroy(&sem->sem_mutex);
 	return -1;
@@ -49,64 +44,40 @@ sem_destroy(sem_t *sem)
     return 0;
 }
 
-static int
-lock(sem_t *sem)
-{
-    if (pthread_mutex_lock(&sem->sem_mutex) == -1)
-    {
-	ici_get_last_errno("mutex_lock", NULL);
-	return -1;
-    }
-    return 0;
-}
-
-static int
-unlock(sem_t *sem)
-{
-    if (pthread_mutex_unlock(&sem->sem_mutex) == -1)
-    {
-	ici_get_last_errno("mutex_unlock", NULL);
-	return -1;
-    }
-    return 0;
-}
-
-static int
-unlock_and_fail(sem_t *sem, const char *what)
-{
-    ici_get_last_errno(what, NULL);
-    (void)pthread_mutex_unlock(&sem->sem_mutex);
-    return -1;
-}
-
 int
 sem_wait(sem_t *sem)
 {
-    if (lock(sem))
+    if (pthread_mutex_lock(&sem->sem_mutex) == -1)
 	return -1;
     ++sem->sem_nwaiters;
     while (sem->sem_count == 0)
     {
 	if (pthread_cond_wait(&sem->sem_condvar, &sem->sem_mutex) == -1)
-	    return unlock_and_fail(sem, "cond_wait");
+	{
+	    (void)pthread_mutex_unlock(&sem->sem_mutex);
+	    return -1;
+	}
     }
     --sem->sem_nwaiters;
     --sem->sem_count;
-    return unlock(sem);
+    return pthread_mutex_unlock(&sem->sem_mutex);
 }
 
 int
 sem_post(sem_t *sem)
 {
-    if (lock(sem))
+    if (pthread_mutex_lock(&sem->sem_mutex) == -1)
 	return -1;
     if (sem->sem_nwaiters > 0)
     {
 	if (pthread_cond_signal(&sem->sem_condvar) == -1)
-	    return unlock_and_fail(sem, "cond_signal");
+	{
+	    (void)pthread_mutex_unlock(&sem->sem_mutex);
+	    return -1;
+	}
     }
     ++sem->sem_count;
-    return unlock(sem);
+    return pthread_mutex_unlock(&sem->sem_mutex);
 }
 
 #endif /* ICI_USE_POSIX_THREADS */
